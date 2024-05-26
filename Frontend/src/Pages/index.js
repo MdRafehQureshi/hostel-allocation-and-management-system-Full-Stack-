c30fcca357353007716276f57df08025c1ca2700
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-05-26 20:27:42 +0530

    add index.js in Pages folder to handle import export

---
+++ Frontend/src/Pages/index.js
@@ -0,0 +1,15 @@
+import ApplicationForm from "./ApplicationForm";
+import Home from "./Home";
+import Instruction from "./Instruction";
+import SignUp from "./SignUp";
+import LogIn from "./LogIn";
+import ApplicationStatus from "./ApplicationStatus";
+
+export {
+    ApplicationForm,
+    Home,
+    Instruction,
+    SignUp,
+    LogIn,
+    ApplicationStatus,
+}
