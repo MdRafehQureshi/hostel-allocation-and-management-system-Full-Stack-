9a6cb1afdf31dc0ec554eb16fa5308d9268d64da
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-05-26 20:19:20 +0530

    add index.js in Component folder to handle import export

---
+++ Frontend/src/Components/index.js
@@ -0,0 +1,17 @@
+import ApplicationFormComp from "./ApplicationFormComp";
+import Input from "./Input";
+import Button from "./Button";
+import File from "./File";
+import Select from "./Select";
+import SignUpComp1 from "./SignUpComp1";
+import LogInComp from "./LogInComp";
+
+export {
+    Input,
+    ApplicationFormComp,
+    Button,
+    File,
+    Select,
+    SignUpComp1,
+    LogInComp
+}
