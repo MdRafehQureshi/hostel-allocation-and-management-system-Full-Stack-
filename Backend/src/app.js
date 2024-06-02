1205dffa66e99b6215783050485736c0bd71917c
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-02 10:18:53 +0530

    add cors origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
 
-app.use(cors)
-app.use(express.json)
-app.use(express.urlencoded({extended:true}))
-app.use(express.static("public"))
-
-
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+    })
+);
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 export { app };
