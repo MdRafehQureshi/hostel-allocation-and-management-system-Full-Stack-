4c91f46a1d79e47d7c06332517337138614784ea
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-02 10:15:15 +0530

    add cors package and setup app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,13 @@
 import express from "express";
+import cors from "cors"
+
 const app = express();
+
+app.use(cors)
+app.use(express.json)
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+
 
 export { app };
