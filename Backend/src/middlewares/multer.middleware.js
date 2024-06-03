cd9b15314a59515396f715748ccc9279b25dbb8f
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-03 12:54:24 +0530

    Change export of upload from default to name,change req.filename to req.fieldname,add multer middleware in the student/appication-form route

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -6,10 +6,10 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-        cb(null, file.filename + "-" + uniqueSuffix);
+        cb(null, file.fieldname + "-" + uniqueSuffix);
     },
 });
 
 const upload = multer({ storage: storage });
 
-export default upload;
+export { upload };
--- Backend/src/routes/student.routes.js
+++ Backend/src/routes/student.routes.js
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { submitApplication } from "../controllers/student.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/application-form").post(submitApplication);
+router.route("/application-form").post(upload.fields([
+    {name: "student_photo", maxCount:1},
+    {name: "student_disability_certificate", maxCount:1},
+    {name: "income_proof", maxCount:1},
+    {name: "admission_proof", maxCount:1},
+    {name: "permanent_address_proof", maxCount:1},
+    {name: "correspondence_address_proof", maxCount:1}
+]),submitApplication);
 
 export default router;
