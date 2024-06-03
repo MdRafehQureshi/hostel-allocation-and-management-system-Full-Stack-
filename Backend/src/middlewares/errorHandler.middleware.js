9d07fb987899185d4dbfe2a970af568e86331fae
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-03 18:07:28 +0530

    add errorHandling middleware

---
+++ Backend/src/middlewares/errorHandler.middleware.js
@@ -0,0 +1,27 @@
+import { ApiError } from "../utils/ApiError.js";
+
+
+function errorHandler(err,req,res,next){
+    if(err instanceof ApiError){
+        if(err.statusCode<500 && err.statusCode>=400){
+            res
+            .status(err.statusCode)
+            .json({
+                success : false ,
+                message : err.message ,
+                errors : err.errors,
+            })
+        }
+        else if(err.statusCode>=500){
+            res
+            .status(err.statusCode)
+            .json({
+                success : false ,
+                message : "Internal Server Error." ,
+                errors : [{message : err.message}]
+            })
+        }
+    }
+}
+
+export { errorHandler }
