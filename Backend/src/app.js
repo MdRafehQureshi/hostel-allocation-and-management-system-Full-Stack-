13000c39a85b38cf7be7d657a933ecc01fab4e78
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-05 13:55:11 +0530

    Create otp route and controller

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
 
 const app = express();
 
@@ -12,12 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-import studentRouter from "./routes/student.routes.js"
+import otpRouter from "./routes/otp.routes.js"
+// import studentRouter from "./routes/student.routes.js"
 
-app.use("/api/v1/student",studentRouter)
+app.use("/api/v1/otp",otpRouter)
+// app.use("/api/v1/student",studentRouter)
 
 
 
+app.use(errorHandler)
 
 export { app };
     
---
+++ Backend/src/routes/otp.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { otpVerification } from "../controllers/otp.controller.js";
+
+const router = Router();
+
+router.route("/get-otp").post(otpVerification)
+
+export default router;
