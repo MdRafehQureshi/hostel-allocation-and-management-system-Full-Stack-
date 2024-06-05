b861abc428d95d98444e408296eb617e4826dee2
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-05 13:56:40 +0530

    Refactor errorHandling middleeware

--- Backend/src/middlewares/errorHandler.middleware.js
+++ Backend/src/middlewares/errorHandler.middleware.js
@@ -3,22 +3,13 @@ import { ApiError } from "../utils/ApiError.js";
 
 function errorHandler(err,req,res,next){
     if(err instanceof ApiError){
-        if(err.statusCode<500 && err.statusCode>=400){
+        if(err.statusCode<=511 || err.statusCode>=400){
             res
             .status(err.statusCode)
             .json({
                 success : false ,
                 message : err.message ,
                 errors : err.errors,
-            })
-        }
-        else if(err.statusCode>=500){
-            res
-            .status(err.statusCode)
-            .json({
-                success : false ,
-                message : "Internal Server Error." ,
-                errors : [{message : err.message}]
             })
         }
     }
