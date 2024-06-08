59f8585e72ae55fe5fcc850bdab1b34c84fe6ac3
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-08 11:07:51 +0530

    add cookie parser middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,13 +12,17 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 app.use(express.static("public"));
 
 import otpRouter from "./routes/otp.routes.js"
-// import studentRouter from "./routes/student.routes.js"
+import authRouter from  "./routes/auth.routes.js"
+import studentRouter from "./routes/student.routes.js"
 
+
+app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/otp",otpRouter)
-// app.use("/api/v1/student",studentRouter)
+app.use("/api/v1/student",studentRouter)
 
 
 
