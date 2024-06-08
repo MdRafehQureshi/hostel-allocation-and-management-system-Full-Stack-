bdd0ed88ea69e116818e4cd49684c2c5cc7ce6ac
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-08 11:06:12 +0530

    add auth route and controller

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,131 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { generateAccessToken, generateRefreshToken } from "../utils/tokens.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { db } from "../db/index.js";
+// import { sendEmail } from "../utils/mail.js";
+// import { generateOtp,hashOtp } from "../utils/otp.js";
+
+const generateAccessAndRefreshTokens = (userId, table) => {
+    const accessToken = generateAccessToken(userId);
+    const refreshToken = generateRefreshToken(userId);
+    if (!accessToken || !refreshToken) {
+        throw new ApiError(
+            500,
+            "Something went wrong while generating access and refresh tokens."
+        );
+    }
+    return { accessToken, refreshToken };
+};
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if ([email, password].some((field) => field.trim() === "")) {
+        throw new ApiError(400, "All fields are required");
+    }
+    const studentLoginData = await db.query(
+        "SELECT email, password, student_id FROM student WHERE email=$1",
+        [email]
+    );
+    if (studentLoginData.rows.length > 0) {
+        const checkPassword = await bcrypt.compare(
+            password,
+            studentLoginData.rows[0].password
+        );
+        if (!checkPassword) {
+            throw new ApiError(400, "Invalid user credentials");
+        }
+        const { accessToken, refreshToken } = generateAccessAndRefreshTokens();
+        try {
+            await db.query(
+                "UPDATE student SET refresh_token=$1 WHERE student_id = $2",
+                [refreshToken, studentLoginData.rows[0].student_id]
+            );
+        } catch (error) {
+            throw new ApiError(
+                500,
+                "Something went wrong while generating access and refresh tokens."
+            );
+        }
+        return res
+            .status(200)
+            .cookie("accessToken", accessToken, {
+                httpOnly: true,
+                secure: true,
+            })
+            .cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                secure: true,
+            })
+            .json(
+                new ApiResponse(
+                    200,
+                    {
+                        user: {
+                            id: studentLoginData.rows[0].student_id,
+                            email: studentLoginData.rows[0].email,
+                            role: "student",
+                        },
+                    },
+                    "User logged in successfully"
+                )
+            );
+    }
+
+    const adminData = await db.query(
+        "SELECT email, password, admin_id, level FROM admin WHERE email=$1",
+        [email]
+    );
+    if (adminData.rows.length < 1) {
+        throw new ApiError(400, "User not found");
+    }
+    const checkPassword = await bcrypt.compare(
+        password,
+        adminData.rows[0].password
+    );
+    if (!checkPassword) {
+        throw new ApiError(400, "Invalid user credentials");
+    }
+    const { accessToken, refreshToken } = generateAccessAndRefreshTokens();
+    try {
+        await db.query(
+            "UPDATE admin SET refresh_token=$1 WHERE admin_id = $2",
+            [refreshToken, adminData.rows[0].admin_id]
+        );
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Something went wrong while generating access and refresh tokens."
+        );
+    }
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: true,
+        })
+        .cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: true,
+        })
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: {
+                        id: adminData.rows[0].admin_id,
+                        email: adminData.rows[0].email,
+                        role: adminData.rows[0].level === "1" ? "admin1" : "admin2",
+                    },
+                },
+                "User logged in successfully"
+            )
+        );
+});
+
+
+const logout = asyncHandler(async (req, res) => {});
+
+export { login, logout };
