c6092868856c250e5f1287b98bdcc92c8c2b3718
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-08 11:21:23 +0530

    deleted otp route and controller files

--- Backend/src/controllers/otp.controller.js
+++
@@ -1,8 +0,0 @@
-// import { db } from "../db/index.js";
-// import { ApiError } from "../utils/ApiError.js";
-// import { asyncHandler } from "../utils/asyncHandler.js";
-// import { sendEmail } from "../utils/mail.js";
-// import { generateOtp, hashOtp } from "../utils/otp.js";
-
-
-// export { otpVerification };
--- Backend/src/routes/otp.routes.js
+++
@@ -1,8 +0,0 @@
-// import { Router } from "express";
-// import { otpVerification } from "../controllers/otp.controller.js";
-
-// const router = Router();
-
-// // router.route("/get-otp").post(otpVerification)
-
-// export default router;
