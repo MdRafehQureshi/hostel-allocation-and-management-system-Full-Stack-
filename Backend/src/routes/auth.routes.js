625b84ea11933efc05a14ae9fbda3c5604d96f96
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-09 18:59:04 +0530

    add verifyJwt authmiddleware function to logout route

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { login,logout,otpVerification } from "../controllers/auth.controller.js";
+import {
+    login,
+    logout,
+    otpVerification,
+} from "../controllers/auth.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/login").post(login)
-router.route("/logout").post(logout)
-router.route("/get-otp").post(otpVerification)
+router.route("/login").post(login);
+router.route("/logout").post(verifyJwt, logout);
+router.route("/get-otp").post(otpVerification);
 
-export default router
+export default router;
