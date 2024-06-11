fb334e360a95cbc36f23421000523e5cc49eff24
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-11 10:04:41 +0530

    Create api layer in frontend and add axios conifg

---
+++ Frontend/src/api/config/axiosConfig.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+
+const apiClient = axios.create({
+    baseURL:"http://localhost:3000/api/v1",
+    headers:{
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+})
+
+export { apiClient }
