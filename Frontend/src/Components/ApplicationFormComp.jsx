25820599044b2ffb5588312d276624f660e2f966
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-18 16:36:18 +0530

    add distance calculation and error handling

--- Frontend/src/Components/ApplicationFormComp.jsx
+++ Frontend/src/Components/ApplicationFormComp.jsx
@@ -4,18 +4,75 @@ import Input from "./Input";
 import Button from "./Button";
 import File from "./File";
 import Select from "./Select";
-import { useEffect } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
+import studentService from "../api/student/student";
+import authService from "../api/auth/auth"; 
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../features/authSlice";
+import { useNavigate } from "react-router-dom";
 
 function ApplicationFormComp() {
   const { control,register, handleSubmit, watch, setValue,formState:{errors} } = useForm();
- 
-  function onSubmit(data) {
-    console.log(data);
-  }
-  
-  const isDisabilityYes = watch('disability') === 'Yes';    //  Check if disability is 'Yes'
+  const [ loading ,setLoading ] = useState(false)
+  const [ error,setError ] = useState(null)
+  const [ addressLoading, setAddressLoading ] = useState(false)
+  const [pAddressErr,setPAddressErr] = useState(null)
+  const [cAddressErr,setCAddressErr] = useState(null)
+  const studentData = useSelector(state => state.auth.userData)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const isDisabilityYes = watch('is_disabled') === 'Yes';    //  Check if disability is 'Yes'
   const selectedDegree = watch('degree');                   //  Get selected degree value
-  
+  const permanentAddress = {
+                              address1: watch("address1"),
+                              city1 : watch("city1"),
+                              district1: watch("district1"),
+                              state1: watch("state1"),
+                              pin_code1: watch("pin_code1"),
+                              country1: watch("country1"),
+                              police_station1: watch("police_station1"),
+                              post_office1: watch("post_office1"),                                                                   
+                             } 
+  const correspondenceAddress = {
+                              country2: watch("country2"),
+                              state2: watch("state2"),
+                              city2 : watch("city2"),
+                              district2: watch("district2"),
+                              address2: watch("address2"),
+                              pin_code2: watch("pin_code2"),
+                              police_station2: watch("police_station2"),
+                              post_office2: watch("post_office2"),                                                                   
+                            }  
+  useLayoutEffect(()=>{
+    (async()=>{
+      try {
+      setError(null)
+      setLoading(true) 
+      const studentData = await studentService.getStudentData()
+      console.log(studentData);
+      dispatch(login(studentData.data))
+      setLoading(false)
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+      if(error.response.status===401){
+        dispatch(logout())
+        navigate("/login")
+      }
+      setError(error)
+    } 
+   })()
+    
+  },[dispatch,navigate])
+
+  useEffect(()=>{
+    if(studentData){
+      setValue("first_name",studentData.first_name)
+      setValue("last_name",studentData.last_name)
+      setValue("email",studentData.email)
+    }
+  },[studentData,setValue])
+
   function validateFileSize(file){
     const maxFileSize = 300 *1024 // 300 KB
     if (file[0].size > maxFileSize) {
@@ -26,19 +83,89 @@ function ApplicationFormComp() {
   
     // Set values to empty strings if disability is 'No'
     
-    useEffect(() => {
+  useEffect(() => {
       if (!isDisabilityYes) {
         setValue('disability_type', ''); // Reset disability type
         setValue('degree_of_disability', ''); // Reset degree of disability
         setValue('student_disability_certificate', ''); // Reset disability certificate
       }
-    }, [isDisabilityYes, setValue]);
+  }, [isDisabilityYes, setValue]);
 
-    useEffect(() => {
+  useEffect(() => {
       const selectedCourse = degreeCourses.find(degree => degree.degree === selectedDegree);
       setValue('course_duration', selectedCourse ? selectedCourse.duration : '');
-    }, [selectedDegree, setValue]);
+  }, [selectedDegree, setValue]);
 
+    async function calculateDistance(addressType){
+      setPAddressErr(null)
+      setCAddressErr(null)
+      let addressData=null
+      if(addressType===1){
+       addressData = permanentAddress
+      }else if(addressType===2){
+       addressData = correspondenceAddress
+      }
+      for(let [key,value] of Object.entries(addressData)){
+        if(value===""|| value===undefined){
+          addressType===1?setPAddressErr(`Permanent ${key.slice(0,-1)} is required.`):setCAddressErr(`Correspondeence ${key.slice(0,-1)} is required`)
+          return
+        }
+      }
+     try {
+        setAddressLoading(true)
+        console.log(addressData);
+        const res = await studentService.getDistance(addressData)
+        const distance = res.data.data.distance.toFixed(2)
+        console.log(distance);
+        setValue("distance"+addressType , distance)
+        setAddressLoading(false)
+     } catch (error) {
+        setAddressLoading(false)
+        addressType===1?setPAddressErr(error):setCAddressErr(error)
+     }
+    }
+    
+    async function onSubmit(data) {
+      try {
+        setError(null);
+        setLoading(true);
+        console.log('before Form data:', data);
+        const formData = new FormData();
+        for (const key in data) {
+          if (data[key] instanceof FileList) {
+            if (data[key].length > 0) {
+              formData.append(key, data[key][0]); // Append the first file
+            }
+          } else {
+            formData.append(key, data[key]);
+          }
+        }
+        for (let [key, value] of formData.entries()) {
+          console.log(`${key}: ${value}`);
+        }
+        const hasSubmit = await studentService.applyForHostel(formData);
+        if (hasSubmit) {
+          setLoading(false);
+          navigate("/student/application-status");
+        }
+      } catch (error) {
+        setLoading(false);
+        setError(error);
+        console.log('Form submission error:', error);
+      }
+    }  
+
+    if(loading || !studentData) {
+      return <Container className="flex items-center justify-center ">Loading..</Container>
+    }
+  
+   if(studentData && studentData.applicant_id){
+    return(<Container className="flex justify-center text-2xl text-center text-gray-500 sm:items-center">
+      <div className="px-4 font-semibold bg-gray-100 shadow-xl py-28 rounded-2xl">
+      Form Already Submitted !!
+      </div>
+    </Container>)
+   }else{
   return (
     <Container className="flex flex-col items-center justify-center">
       <div className="w-full pb-5 shadow-md sm:w-2/5">
@@ -49,10 +176,10 @@ function ApplicationFormComp() {
           
           <h2 className="my-2 text-xl font-medium tracking-wide text-gray-600 "> Student Details</h2>
           
-          <Input type={"text"} label={"fisst name"} {...register("first_name")} />
-          <Input type={"text"} label={"last Name"} {...register("last_name")} />
-          <Input type={"email"} label={"Email"} {...register("email")} />
-          <Input type={"text"} label={"Student Phone Number"} {...register("student_pohne_number")} />
+          <Input type={"text"} label={"fisst name"} {...register("first_name")} readOnly/>
+          <Input type={"text"} label={"last Name"} {...register("last_name")} readOnly />
+          <Input type={"email"} label={"Email"} {...register("email")} readOnly/>
+          <Input type={"text"} label={"Student Phone Number"} {...register("student_phone_number")} />
           <Select label={"Gender"} options={['Male','Female','Others']} {...register("gender")} />
           <Input type={"date"} label={"Date of Birth"} {...register("date_of_birth")} />
                        
@@ -94,7 +221,7 @@ function ApplicationFormComp() {
                       {/* Student Medical Details */}
           <h2 className="mt-5 mb-3 text-xl font-medium tracking-wide text-gray-600 ">Student Medical Details </h2>
           
-          <Select label={"Disability"} options={['Yes','No']} {...register("disability")} />
+          <Select label={"Disability"} options={['Yes','No']} {...register("is_disabled")} />
           <Select label={"Disability Type"} options={disabilityList} {...register("disability_type")} disabled={!isDisabilityYes} />
           <Input type={"text"} label={"Degree of Disability"} {...register("degree_of_disability")} disabled={!isDisabilityYes} />
           <Select label={"Blood Group"} options={["A+", "A-", "B+", "B-","AB+","AB-","O+","O-"]} {...register("blood_group")} />
@@ -112,29 +239,69 @@ function ApplicationFormComp() {
                       {/*Permanent Address*/}
           <h2 className="mt-5 mb-3 text-xl font-medium tracking-wide text-gray-600 ">Permanent Address</h2>
 
-          <Input type={"text"} label={"Country1"} {...register("country1")} />
-          <Input type={"text"} label={"State1"} {...register("state1")} />
-          <Input type={"text"} label={"City1"} {...register("city1")} />
-          <Input type={"text"} label={"District1"} {...register("district1")} />
-          <Input type={"text"} label={"Address1"} {...register("address1")} />
-          <Input type={"text"} label={"Pin Code1"} {...register("pin_code1")} />
-          <Input type={"text"} label={"Police Station1"} {...register("police_station1")} />
-          <Input type={"text"} label={"Post Office1"} {...register("post_office1")} />
-          <Input type={"text"} label={"Distance1"} {...register("distance1")} />
-          
+          <Input type={"text"} label={"Permanent Country"} {...register("country1")} />
+          <Input type={"text"} label={"Permanent State"} {...register("state1")} />
+          <Input type={"text"} label={"Permanent City"} {...register("city1")} />
+          <Input type={"text"} label={"Permanent District"} {...register("district1")} />
+          <Input type={"text"} label={"Permanent Address"} {...register("address1")} />
+          <Input type={"text"} label={"Permanent Pin Code"} {...register("pin_code1")} />
+          <Input type={"text"} label={"Permanent Police Station"} {...register("police_station1")} />
+          <Input type={"text"} label={"Permanent Post Office"} {...register("post_office1")} />
+          {/* <input type={"text"} label={"Permanent Distance"} /> */}
+          <div className="flex w-full pl-5 ">
+            <div className="flex flex-col pr-3">
+                <label className="mb-1 text-base text-gray-700 capitalize" htmlFor={"distance1"}>
+                  Permanent Distance
+                </label>
+                <input
+                  id={"distance1"}
+                  {...register("distance1")} 
+                  className={`border rounded-lg text-sm border-gray-400 bg-white h-9 text-gray-800 px-3 py-2 outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  duration-200 `}
+                  readOnly
+              / >
+            </div>
+            <Button onClick={async ()=> await calculateDistance(1)} className="w-3/6 py-2 mt-5 mr-3 text-center text-white duration-300 bg-gray-600 rounded-md sm:mr-0 sm:py-1 active:opacity-80 sm:active:hover:scale-110 ">
+            {addressLoading?"Calculating..":"Calculate Distance"}
+            </Button>
+          </div>
+          {pAddressErr && (
+            <p className="mt-1 text-xs font-medium text-red-600">
+              {pAddressErr?.response?.data?.message||pAddressErr}
+            </p>
+          )}
                      {/*Correspondence Address*/}
           <h2 className="mt-5 mb-3 text-xl font-medium tracking-wide text-gray-600 ">Correspondence Address</h2>
 
-          <Input type={"text"} label={"Country2"} {...register("country2")} />
-          <Input type={"text"} label={"State2"} {...register("state2")} />
-          <Input type={"text"} label={"City2"} {...register("city2")} />
-          <Input type={"text"} label={"District2"} {...register("district2")} />
-          <Input type={"text"} label={"Address2"} {...register("address2")} />
-          <Input type={"text"} label={"Pin Code2"} {...register("pin_code2")} />
-          <Input type={"text"} label={"Police Station2"} {...register("police_station2")} />
-          <Input type={"text"} label={"Post Office2"} {...register("post_office2")} />
-          <Input type={"text"} label={"Distance2"} {...register("distance2")} />
-          
+          <Input type={"text"} label={"Correspondence Country"} {...register("country2")} />
+          <Input type={"text"} label={"Correspondence State"} {...register("state2")} />
+          <Input type={"text"} label={"Correspondence City"} {...register("city2")} />
+          <Input type={"text"} label={"Correspondence District"} {...register("district2")} />
+          <Input type={"text"} label={"Correspondence Address"} {...register("address2")} />
+          <Input type={"text"} label={"Correspondence Pin Code"} {...register("pin_code2")} />
+          <Input type={"text"} label={"Correspondence Police Station"} {...register("police_station2")} />
+          <Input type={"text"} label={"Correspondence Post Office"} {...register("post_office2")} />
+          {/* <Input type={"text"} label={"Correspondence Distance"} {...register("distance2")} /> */}
+          <div className="flex w-full pl-5 ">
+            <div className="flex flex-col pr-3">
+                <label className="mb-1 text-base text-gray-700 capitalize" htmlFor={"distance2"}>
+                  Correspondence Distance
+                </label>
+                <input
+                  id={"distance2"}
+                  {...register("distance2")} 
+                  className={`border rounded-lg text-sm border-gray-400 bg-white h-9 text-gray-800 px-3 py-2 outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  duration-200 `}
+                  readOnly
+              / >
+            </div>
+            <Button onClick={async ()=> await calculateDistance(2)} className="w-3/6 py-2 mt-5 mr-3 text-center text-white duration-300 bg-gray-600 rounded-md sm:mr-0 sm:py-1 active:opacity-80 sm:active:hover:scale-110 ">
+            {addressLoading?"Calculating..":"Calculate Distance"}
+            </Button>
+          </div>
+          {cAddressErr && (
+            <p className="mt-1 text-xs font-medium text-red-600">
+              {cAddressErr?.response?.data?.message||cAddressErr}
+            </p>
+          )}
 
                       {/*Documents Upload*/} 
           <h2 className="mt-5 mb-3 text-xl font-medium tracking-wide text-gray-600 ">Documents Upload</h2>
@@ -158,7 +325,7 @@ function ApplicationFormComp() {
         </form>
       </div>
     </Container>
-  );
+  );}
 }
 
 export default ApplicationFormComp;
@@ -279,5 +446,3 @@ const degreeCourses = [
       "Software Engineering"
   ] }
 ];
-
-
