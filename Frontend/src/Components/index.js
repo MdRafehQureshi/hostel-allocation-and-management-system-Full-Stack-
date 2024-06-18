11ba0e80b4956b15338e5226782f84ef219b8406
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-18 16:12:38 +0530

    Add application status import export

--- Frontend/src/Components/index.js
+++ Frontend/src/Components/index.js
@@ -1,18 +1,21 @@
 import ApplicationFormComp from "./ApplicationFormComp";
+import SignUpComp1 from "./SignUpComp1";
+import LogInComp from "./LogInComp";
+import ApplicationStatusComp from "./ApplicationStatusComp";
 import Input from "./Input";
 import Button from "./Button";
 import File from "./File";
 import Select from "./Select";
-import SignUpComp1 from "./SignUpComp1";
-import LogInComp from "./LogInComp";
 import AuthLayout from "./AuthLayout";
+
 export {
-    Input,
     ApplicationFormComp,
+    ApplicationStatusComp ,
+    SignUpComp1,
+    LogInComp,
+    Input,
     Button,
     File,
     Select,
-    SignUpComp1,
-    LogInComp,
     AuthLayout,
 }
