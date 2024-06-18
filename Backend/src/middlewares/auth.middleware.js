9c317f90d0ae1b1ec65102ae2dace919de50e6b5
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-18 16:34:53 +0530

    Remove log

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -8,7 +8,6 @@ const verifyJwt = asyncHandler(async (req, res, next) => {
         const token = req.cookies?.accessToken;
         if (!token) throw new ApiError(401, "Unauthorized user!");
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        console.log("Decoded token", decodedToken);
         const student = await db.query(
             "SELECT student_id, first_name, last_name, email, student_phone_number, gender, date_of_birth, degree, course_name, course_duration, admission_year, current_semester, is_disabled, disability_type, degree_of_disability, blood_group, guardian_full_name, guardian_contact_number, relation_with_guardian, guardian_occupation, annual_family_income, country1, state1, city1, district1, address1, pin_code1, police_station1, post_office1, distance1, country2, state2, city2, district2, address2, pin_code2, police_station2, post_office2, distance2, student_photo, student_disability_certificate, admission_proof, income_proof, permanent_address_proof, correspondence_address_proof, resident_id, applicant_id,resident_id FROM student WHERE student_id=$1",
             [decodedToken?.id]
