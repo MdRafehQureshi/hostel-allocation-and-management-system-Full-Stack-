88f02bb9c993876b144836ef865e16272f908469
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-20 22:58:59 +0530

    Add updateAdminEmail route and controller

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -1,5 +1,8 @@
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { verifyOtp } from "../utils/otp.js";
+import {db} from "../db/index.js"
 
 const getCurrentAdmin = asyncHandler(async (req, res) => {
     //if admin1 fetch all data from all hostel
@@ -16,4 +19,50 @@ const getCurrentAdmin = asyncHandler(async (req, res) => {
         );
 });
 
-export {getCurrentAdmin}
+const updateAdminEmail = asyncHandler(async (req,res)=>{
+   try {
+     console.log(req.body);
+     const {old_email, old_email_otp, new_email, new_email_otp} = req.body
+     
+     const verifyOldEmail = await db.query("SELECT * FROM otp WHERE email = $1",[old_email])
+     console.log("old otp res",verifyOldEmail.rows[0])
+     if(verifyOldEmail.rows.length<1){
+         throw new ApiError(404,"Could not verify the current email.")
+     }
+     if(!verifyOldEmail.rows[0].otp){
+         throw new ApiError(404,"Please generate OTP for current email.")
+     }
+     if(!verifyOtp(old_email_otp,verifyOldEmail.rows[0].otp)){
+         throw new ApiError(400,"Incorrect OTP")
+     }
+     if(req.user.email !== old_email ){
+         throw new ApiError(400,"Current email is incorrect.")
+     }
+     await db.query("DELETE FROM otp WHERE email=$1",[old_email])
+ 
+     const verifyNewEmail = await db.query("SELECT * FROM otp WHERE email = $1",[new_email])
+ 
+     if(verifyNewEmail.rows.length<1){
+         throw new ApiError(404,`Could not verify  ${new_email}`)
+     }
+     if(!verifyNewEmail.rows[0].otp){
+         throw new ApiError(404,`Please generate OTP for ${new_email}.`)
+     }
+     if(!verifyOtp(new_email_otp,verifyNewEmail.rows[0].otp)){
+         throw new ApiError(400,"Incorrect OTP")
+     }
+     await db.query("DELETE FROM otp WHERE email=$1",[new_email])
+ 
+     const response = await db.query("UPDATE admin SET email=$1 WHERE email=$2 RETURNING email",[new_email,old_email])
+ 
+     if(response.rows[0].email !== new_email){
+         throw new ApiError(400,"Could not update the email")
+     }
+     return res.status(200).json(new ApiResponse(200,{},"Email updated successfully"))
+   } catch (error) {
+        console.log(error)
+   }
+})
+
+
+export {getCurrentAdmin, updateAdminEmail}
--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { restrictTo } from "../middlewares/restriction.middleware.js";
-import { getCurrentAdmin } from "../controllers/admin.controller.js";
+import { getCurrentAdmin, updateAdminEmail } from "../controllers/admin.controller.js";
 
 const router = Router()
 
 router.route("/get-current-admin").get(verifyJwt,restrictTo(["admin1","admin2"]),getCurrentAdmin)
+router.route("/update-admin-email").post(verifyJwt,restrictTo(["admin1","admin2"]),updateAdminEmail)
+
 
 export default router
