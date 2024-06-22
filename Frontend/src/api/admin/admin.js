00ebaa9104833e78e3409cf6202cffd4fa0f0239
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-22 12:21:04 +0530

    Add api call of update admin email

--- Frontend/src/api/admin/admin.js
+++ Frontend/src/api/admin/admin.js
@@ -4,6 +4,10 @@ class Admin {
     async getAdminData(){
         return await apiClient.get("/api/v1/admin/get-current-admin")
     }
+
+    async updateAdminEmail(data){
+        return await apiClient.post("/api/v1/admin/update-admin-email",data)
+    }
 }
 
 const adminApiService = new Admin()
