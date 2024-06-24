773efd70a0c1f33dab2193c78a54a3bd51feb508
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-24 11:27:46 +0530

    Add allotment , addhostel,addRoom,getAllRooms,getAllhostels  api calls

--- Frontend/src/api/admin/admin.js
+++ Frontend/src/api/admin/admin.js
@@ -8,6 +8,26 @@ class Admin {
     async updateAdminEmail(data){
         return await apiClient.post("/api/v1/admin/update-admin-email",data)
     }
+
+    async runAllocation(){
+        return await apiClient.post("/api/v1/allot")
+    }
+
+    async addHostel(data){
+        return await apiClient.post("api/v1/hostel",data)
+    }
+
+    async addRooms(data){
+        return await apiClient.post("api/v1/rooms",data)
+    }
+
+    async getAllhostels(){
+        return await apiClient.get("api/v1/hostels")
+    }
+
+    async getAllrooms(){
+        return await apiClient.get("api/v1/all-rooms")
+    }
 }
 
 const adminApiService = new Admin()
