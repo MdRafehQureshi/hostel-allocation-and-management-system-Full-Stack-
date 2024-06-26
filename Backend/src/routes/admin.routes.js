6ca2892ca52e4856b9b353272f05e721be32ff55
Md Rafeh Qureshi <mdrafehqureshi@gmail.com>
2024-06-26 05:45:02 +0530

    add allot rooms,get all hostel,get all rooms,get all active residents,get all active applicants routes

--- Backend/src/routes/admin.routes.js
+++ Backend/src/routes/admin.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import { restrictTo } from "../middlewares/restriction.middleware.js";
-import { test, getCurrentAdmin, updateAdminEmail, addHostel, addRoom, allotRooms, getAllHostels, getAllRooms } from "../controllers/admin.controller.js";
+import { test, getCurrentAdmin, updateAdminEmail, addHostel, addRoom, allotRooms, getAllHostels, getAllRooms, getAllActiveResidents, getAllActiveApplicants } from "../controllers/admin.controller.js";
 
 const router = Router()
 
@@ -11,6 +11,8 @@ router.route("/hostel").post(verifyJwt,restrictTo(["admin1"]),addHostel)
 router.route("/hostels").get(verifyJwt,restrictTo(["admin1"]),getAllHostels)
 router.route("/rooms").post(verifyJwt,restrictTo(["admin1"]),addRoom)
 router.route("/all-rooms").get(verifyJwt,restrictTo(["admin1"]),getAllRooms)
+router.route("/all-active-residents").get(verifyJwt,restrictTo(["admin1","admin2"]),getAllActiveResidents)
+router.route("/all-active-applicants").get(verifyJwt,restrictTo(["admin1"]),getAllActiveApplicants)
 router.route("/test").post(test)
 router.route("/allot").post(verifyJwt,restrictTo(["admin1"]),allotRooms)
 
